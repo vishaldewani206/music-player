e2bf30384cc8f4576642ea62ed9c25cec8c7a49e
Vishal <dewani.vishal206@gmail.com>
2022-11-07 16:25:28 +0500

    almost complete

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ function pad(unit) {
   return unit < 10 ? '0' + unit : unit;
   unit = unit.toString();
 }
+//lists of songs
+let songList = [
+  'songs/Benny Blanco, Halsey & Khalid - Eastside (Lyrics) (1).mp3',
+  'songs/Juice Wrld - Lucid Dreams (Lyrics).mp3',
+  'songs/Post Malone - rockstar (Lyrics) ft. 21 Savage.mp3',
+  'songs/Somebody That I Used To Know - Gotye (Lyrics) ft. Kimbra.mp3',
+  'songs/Taio Cruz - Dynamite (Lyrics).mp3',
+  'songs/Arizona Zervas - ROXANNE (Lyrics).mp3',
+  'songs/Bazanji - Fed Up (Lyrics).mp3',
+  'songs/CJ - Whoopty (Lyrics).mp3',
+  'songs/Glass Animals - Heat Waves (Lyrics).mp3',
+  'songs/Harry Styles - Watermelon Sugar (Lyrics).mp3',
+  'songs/Jos Slovick - I Am a Poor Wayfaring Stranger (from 1917) - Official Video.mp3',
+  'songs/Lil Nas X - SUN GOES DOWN (Lyrics).mp3',
+  'songs/MAGIC! - Rude (Lyrics).mp3',
+  'songs/Post Malone, Swae Lee - Sunflower (Spider-Man - Into the Spider-Verse).mp3',
+  'songs/Travis Scott - SICKO MODE ft. Drake.mp3',
+  'songs/Vance Joy - Riptide (Lyrics).mp3',
+];
 let volume = document.querySelector('#volume_range');
 let songForward = false;
+let songNumber = 0;
 //adding song
-let audioCtx = new Audio('songs/Akon - Right Now (Na Na Na) (Lyrics).mp3');
+let audioCtx = new Audio(songList[songNumber]);
 audioCtx.volume = volume.value;
 let songSlider = document.querySelector('#slider_btn');
 
@@ -32,15 +52,16 @@ let play = document.querySelector('.on');
 let songLength = document.querySelector('.ending_time');
 let startingSeconds;
 let startingMinutes;
-play.parentElement.addEventListener('click', () => {
+play.parentElement.addEventListener('click', playAndPause);
+function playAndPause() {
   if (k) {
     play.src = 'images/play.png';
-
+    audioCtx.volume = volume.value;
     audioCtx.play();
-    timer = setInterval(startingTime, 1000);
     k = false;
     //volume length
     let length = audioCtx.duration;
+
     let minutes = Math.round(length / 60);
     let seconds = Math.round(length % 60);
 
@@ -49,6 +70,7 @@ play.parentElement.addEventListener('click', () => {
     minutes = pad(minutes);
     seconds = pad(seconds);
     songLength.innerHTML = `${minutes}:${seconds}`;
+    timer = setInterval(startingTime, 1000);
 
     //slider
   } else {
@@ -57,15 +79,15 @@ play.parentElement.addEventListener('click', () => {
     audioCtx.pause();
     clearInterval(timer);
   }
-});
+}
 
 //volume range gradient and increase decrease volume
 volume.addEventListener('input', () => {
   let math = volume.value * 100 + '%';
-  console.log(math);
+
   audioCtx.volume = volume.value;
   volume.style.background =
-    ' linear-gradient(90deg, rgba(74,76,80,1) 0%, rgba(74,76,80,1)' +
+    ' linear-gradient(90deg, rgb(23, 23, 24) 0%, rgb(23, 23, 24)' +
     math +
     ', rgba(209,209,210,0)' +
     math +
@@ -79,7 +101,7 @@ let length;
 function startingTime() {
   startingSeconds = Math.round(audioCtx.currentTime % 60);
   startingMinutes = Math.floor(audioCtx.currentTime / 60);
-  console.log(`starting minutes: ${startingMinutes}`);
+
   if (startingSeconds > 59) {
     startingSeconds = 0;
     startingMinutes = startingMinutes + 1;
@@ -95,13 +117,12 @@ function startingTime() {
     currenttime = audioCtx.currentTime;
   }
   songSlider.value = (currenttime / length) * 100;
-  console.log(songSlider.value);
 }
 
 //songslider forwarding
 songSlider.addEventListener('input', () => {
   currenttime = Math.round((songSlider.value * length) / 100);
-  console.log(currenttime);
+
   //song pause
   play.src = 'images/pause.png';
   k = true;
@@ -111,3 +132,90 @@ songSlider.addEventListener('input', () => {
 });
 
 console.log('fuck you i am in ADDINGSONGS branch');
+
+//hamburger
+//display song playlist
+let songPlaylist = document.querySelector('.songs');
+let hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  if (hamburger.classList == 'hamburger active') {
+    songPlaylist.style.opacity = 1;
+    songPlaylist.style.zIndex = 1;
+  } else {
+    songPlaylist.style.opacity = 0;
+    songPlaylist.style.zIndex = -1;
+  }
+});
+
+//adding song name
+let songNameDiv = document.querySelector('.songName');
+let songName = songList[songNumber].split('songs/');
+songName = songName[1].split('.mp3');
+
+songNameDiv.innerHTML = songName[0];
+
+//backward forward songs
+let forward = document.querySelector('.forward');
+let backward = document.querySelector('.backward');
+backward.addEventListener('click', () => {
+  songNumber--;
+  if (songNumber < 0) {
+    songNumber = songList.length - 1;
+  }
+  audioCtx.pause();
+  audioCtx = new Audio(songList[songNumber]);
+
+  k = true;
+  length = audioCtx.duration;
+
+  playAndPause();
+  let songNameDiv = document.querySelector('.songName');
+  let songName = songList[songNumber].split('songs/');
+  songName = songName[1].split('.mp3');
+
+  songNameDiv.innerHTML = songName[0];
+});
+forward.addEventListener('click', nextSong);
+function nextSong() {
+  songNumber++;
+  if (songNumber >= songList.length - 1) {
+    songNumber = 0;
+  }
+  console.log('songnumber' + songList.length);
+  audioCtx.pause();
+  audioCtx = new Audio(songList[songNumber]);
+
+  k = true;
+  length = audioCtx.duration;
+
+  clearInterval(timer);
+  playAndPause();
+  let songNameDiv = document.querySelector('.songName');
+  let songName = songList[songNumber].split('songs/');
+  songName = songName[1].split('.mp3');
+
+  songNameDiv.innerHTML = songName[0];
+}
+
+for (let i = 0; i < songList.length; i++) {
+  let songName = songList[i].split('songs/');
+  songName = songName[1].split('.mp3');
+
+  songPlaylist.innerHTML +=
+    '<h2 class="playlistSongs">' + songName[0] + '</h2>';
+}
+let playlistSongs = document.querySelectorAll('.playlistSongs');
+
+document.addEventListener('click', () => {
+  for (let j = 0; j < playlistSongs.length; j++) {
+    playlistSongs[j].addEventListener('click', () => {
+      audioCtx.pause();
+      audioCtx = new Audio('songs/' + playlistSongs[j].textContent + '.mp3');
+
+      k = true;
+      playAndPause();
+      songNameDiv.innerHTML = playlistSongs[j].textContent;
+    });
+  }
+});
